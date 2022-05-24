66ad1b2f94b44cf4aead7ae9877360981f1386f4
R14GENIE <ruben4961.g@gmail.com>
2022-05-24 10:53:06 +0200

    Update main.ino

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,9 +1,67 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+   Made By R14GENIE
 
+   Free to use
+
+   Using a HC-06 Bluethooth module
+
+   V.1.0:
+   + Make connexion with any device and turn on a led with a port
+
+*/
+
+/*
+  Include
+*/
+#include <SoftwareSerial.h>   // librairie pour creer une nouvelle connexion serie max 9600 baud
+
+/*
+  Define
+*/
+#define PIN_LED 7
+
+/*
+  Assign pin for the bluetooth module
+*/
+SoftwareSerial BTSerial(10, 11); // RX | TX  = > BT-TX=10 BT-RX=11
+
+
+void setup()
+{
+  // Init Serial
+  Serial.begin(9600);
+  Serial.println("Setup begin");
+
+  // Init the BTSerial at 9600 baud
+  BTSerial.begin(9600);
+
+  //Init the PIN for the led as OUTPUT
+  pinMode(PIN_LED, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
 
+  /*
+   * Variable
+  */
+  String message;
+    
+  while (Serial.available()) {
+    
+    message = Serial.readString();
+    BTSerial.println(message);
+    
+  }
+
+  if (message == "on\r\n") {
+    
+    digitalWrite(PIN_LED, HIGH); 
+    
+  }
+  else if (message == "off\r\n") {
+    
+    digitalWrite(PIN_LED, LOW); 
+    
+  }
 }
